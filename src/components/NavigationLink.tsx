cfbadd2de6f224f33a4aac31c214495ec5023bad
Kiur Jullis <kiur.jullis@gmail.com>
2024-05-26 09:09:17 +0300

    feat: Update NavigationLink component for improved accessibility and mobile responsiveness

--- src/components/NavigationLink.tsx
+++ src/components/NavigationLink.tsx
@@ -1,4 +1,4 @@
-'use client';
+/*'use client';
 
 import clsx from 'clsx';
 import { useSelectedLayoutSegment } from 'next/navigation';
@@ -25,4 +25,34 @@ export default function NavigationLink<Pathname extends AppPathnames>({
       {...rest}
     />
   );
+}*/
+
+'use client';
+
+import { useSelectedLayoutSegment } from 'next/navigation';
+import { ComponentProps } from 'react';
+import { cn } from '../lib/utils';
+import { Link } from '../navigatsion';
+import type { AppPathnames } from '../config';
+
+export default function NavigationLink<Pathname extends AppPathnames>({
+  href,
+  ...rest
+}: ComponentProps<typeof Link<Pathname>>) {
+  const selectedLayoutSegment = useSelectedLayoutSegment();
+  const pathname = selectedLayoutSegment ? `/${selectedLayoutSegment}` : '/';
+  const isActive = pathname === href;
+
+  return (
+    <Link
+      aria-current={isActive ? 'page' : undefined}
+      className={cn(
+        'inline-block px-2 py-3 transition-colors',
+        isActive ? 'text-white' : 'text-gray-400 hover:text-gray-200'
+      )}
+      href={href}
+      {...rest}
+    />
+  );
 }
+
