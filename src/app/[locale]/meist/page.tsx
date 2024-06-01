699312c55471a36b7e18c266c9f735d55c49624c
Kiur Jullis <kiur.jullis@gmail.com>
2024-06-01 09:26:35 +0300

    refactor: Update page layout to use Hero component for better visual presentation

--- src/app/[locale]/meist/page.tsx
+++ src/app/[locale]/meist/page.tsx
@@ -1,6 +1,6 @@
 import { useTranslations } from 'next-intl';
 import { unstable_setRequestLocale } from 'next-intl/server';
-import PageLayout from '@/components/PageLayout';
+import Hero from '@/components/Hero';
 
 type Props = {
   params: { locale: string };
@@ -13,9 +13,15 @@ export default function MeistPage({ params: { locale } }: Props) {
   const t = useTranslations('MeistPage');
 
   return (
-    <div>
+    <>
+      <Hero title="Great companies are made by great people" subtitle='Vivamus venenatis, lectus a pellentesque placerat, felis tellus ultrices eros, et aliquam sapien nibh sed sem. Fusce ut ante eget purus viverra malesuada. Morbi eget mi maximus, ultricies urna eget, tristique lorem. Fusce cursus venenatis quam, a blandit lectus vulputate id. Praesent id interdum orci, vitae condimentum dui.' tagline='Puhkekeskus'
+        callToAction='Tere' callToAction2='Tere' image={{
+          src: '/img/jurta5.jpg',
+          alt: 'Tere', blurDataURL: 'yourBase64EncodedImage'
+        }}
+      />
       <h1>{t('title')}</h1>
       <p>{t('description')}</p>
-    </div>
+    </>
   );
 }
