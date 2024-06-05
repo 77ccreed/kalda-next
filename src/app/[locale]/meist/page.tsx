2881c7a40d827e172b60c5efe1da35625e46dfcb
Kiur Jullis <kiur.jullis@gmail.com>
2024-06-05 10:05:42 +0300

    refactor: Add background image to MeistPage using lazy loading for improved performance

--- src/app/[locale]/meist/page.tsx
+++ src/app/[locale]/meist/page.tsx
@@ -1,6 +1,7 @@
 import { useTranslations } from 'next-intl';
 import { unstable_setRequestLocale } from 'next-intl/server';
 import Hero from '@/components/Hero';
+import Img from '@/assets/images/taust.jpg';
 
 type Props = {
   params: { locale: string };
@@ -23,7 +24,7 @@ export default function MeistPage({ params: { locale } }: Props) {
         callToAction2="Tutvu"
         callToActionLink2="/about"   // Link for the second CTA button
         image={{
-          src: '/img/jurta5.jpg',
+          src: Img.src,
           alt: 'Tere',
           blurDataURL: 'yourBase64EncodedImage'
         }}
