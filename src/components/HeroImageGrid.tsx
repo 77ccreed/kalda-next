c470bf8e2fe50c2aa6a065ae9daeec552389eb57
Kiur Jullis <kiur.jullis@gmail.com>
2024-07-01 10:24:03 +0300

    refactor: Update Hero, Footer, Content, and Comparison components with improved styles and layouts

--- src/components/HeroImageGrid.tsx
+++ src/components/HeroImageGrid.tsx
@@ -34,112 +34,116 @@ const HeroImageGrid = ({
         <div className="absolute left-0 top-1 h-[20500px] w-[20500px] translate-x-[-47.5%] rounded-full bg-primary" />
       </div>
       <div className="container relative flex flex-col px-4">
-        <div className="mx-auto max-w-7xl px-4 pt-24 sm:px-6 md:flex md:min-h-screen">
-          <div className="block py-12 text-center md:flex md:py-12 md:text-left lg:py-16">
-            <div className="mx-auto flex max-w-5xl basis-[56%] items-center">
-              <div className="max-w-3xl pb-12 pr-0 md:py-0 md:pr-8 md:pb-0 lg:pr-16">
-                {tagline && (
-                  <p className="text-base font-semibold uppercase tracking-wide text-secondary">
-                    {tagline}
-                  </p>
+        <div className="mx-auto max-w-7xl px-4 pt-24 sm:px-6 md:pt-32 lg:pt-40">
+          <div className="text-center md:text-left">
+            <div className="mx-auto max-w-3xl md:mb-8">
+              {tagline && (
+                <p className="text-base font-semibold uppercase tracking-wide text-secondary">
+                  {tagline}
+                </p>
+              )}
+              {title && (
+                <h1 id="hero-title" className="leading-tighter font-heading mb-4 text-4xl font-bold tracking-tight md:text-5xl text-primary-foreground">
+                  {title}
+                </h1>
+              )}
+              {subtitle && (
+                <p className="mb-8 text-lg font-normal text-primary-foreground">
+                  {subtitle}
+                </p>
+              )}
+              <div className="flex flex-col sm:flex-row justify-center md:justify-start gap-4">
+                {callToAction && callToActionLink && (
+                  <Button asChild variant="secondary" className="shadow-lg transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                    <NavigationLink href={callToActionLink}>
+                      {callToAction}
+                    </NavigationLink>
+                  </Button>
                 )}
-                {title && (
-                  <h1 id="hero-title" className="leading-tighter font-heading mb-4 text-4xl font-bold tracking-tight md:text-5xl text-primary-foreground">
-                    {title}
-                  </h1>
+                {callToAction2 && callToActionLink2 && (
+                  <Button asChild variant="highlight" className="shadow-lg transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                    <NavigationLink href={callToActionLink2}>
+                      {callToAction2}
+                    </NavigationLink>
+                  </Button>
                 )}
-                <div className="mx-auto max-w-3xl">
-                  {subtitle && (
-                    <p className="mb-8 text-lg font-normal text-primary-foreground">
-                      {subtitle}
-                    </p>
-                  )}
-                  <div className="flex flex-col sm:flex-row justify-center md:justify-start gap-4">
-                    {callToAction && callToActionLink && (
-                      <Button asChild variant="secondary" className="shadow-lg transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
-                        <NavigationLink href={callToActionLink}>
-                          {callToAction}
-                        </NavigationLink>
-                      </Button>
-                    )}
-                    {callToAction2 && callToActionLink2 && (
-                      <Button asChild variant="highlight" className="shadow-lg transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
-                        <NavigationLink href={callToActionLink2}>
-                          {callToAction2}
-                        </NavigationLink>
-                      </Button>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="block flex-1 items-center md:flex">
-              <div className="relative m-auto h-full max-w-4xl">
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                  <AspectRatio ratio={3 / 2} className="relative w-full">
-                    <Image
-                      src={Img1}
-                      alt="Telkimisniit"
-                      layout="fill"
-                      className="rounded-md object-cover drop-shadow-2xl"
-                      sizes="(min-width: 1920px) 749px, (min-width: 1540px) 43.89vw, (min-width: 1360px) 542px, (min-width: 780px) calc(39.29vw + 16px), calc(96.52vw - 22px)"
-                      priority
-                    />
-                  </AspectRatio>
-                  <AspectRatio ratio={3 / 2} className="relative w-full">
-                    <Image
-                      src={Img2}
-                      alt="Autoelamud"
-                      layout="fill"
-                      className="rounded-md object-cover drop-shadow-2xl"
-                      sizes="(min-width: 1920px) 749px, (min-width: 1540px) 43.89vw, (min-width: 1360px) 542px, (min-width: 780px) calc(39.29vw + 16px), calc(96.52vw - 22px)"
-                      priority
-                    />
-                  </AspectRatio>
-                  <AspectRatio ratio={4 / 3} className="relative w-full">
-                    <Image
-                      src={Img3}
-                      alt="Kämpingumaja"
-                      layout="fill"
-                      className="rounded-md object-cover drop-shadow-2xl"
-                      sizes="(min-width: 1920px) 749px, (min-width: 1540px) 43.89vw, (min-width: 1360px) 542px, (min-width: 780px) calc(39.29vw + 16px), calc(96.52vw - 22px)"
-                      priority
-                    />
-                  </AspectRatio>
-                  <AspectRatio ratio={4 / 3} className="relative w-full">
-                    <Image
-                      src={Img4}
-                      alt="Lihtne öömaja"
-                      layout="fill"
-                      className="rounded-md object-cover drop-shadow-2xl"
-                      sizes="(min-width: 1920px) 749px, (min-width: 1540px) 43.89vw, (min-width: 1360px) 542px, (min-width: 780px) calc(39.29vw + 16px), calc(96.52vw - 22px)"
-                      priority
-                    />
-                  </AspectRatio>
-                  <AspectRatio ratio={4 / 3} className="relative w-full">
-                    <Image
-                      src={Img5}
-                      alt="Kodumajutuse toad"
-                      layout="fill"
-                      className="rounded-md object-cover drop-shadow-2xl"
-                      sizes="(min-width: 1920px) 749px, (min-width: 1540px) 43.89vw, (min-width: 1360px) 542px, (min-width: 780px) calc(39.29vw + 16px), calc(96.52vw - 22px)"
-                      priority
-                    />
-                  </AspectRatio>
-                  <AspectRatio ratio={4 / 3} className="relative w-full">
-                    <Image
-                      src={Img6}
-                      alt="Puhkemaja"
-                      layout="fill"
-                      className="rounded-md object-cover drop-shadow-2xl"
-                      sizes="(min-width: 1920px) 749px, (min-width: 1540px) 43.89vw, (min-width: 1360px) 542px, (min-width: 780px) calc(39.29vw + 16px), calc(96.52vw - 22px)"
-                      priority
-                    />
-                  </AspectRatio>
-                </div>
               </div>
             </div>
           </div>
+        </div>
+        <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          <AspectRatio ratio={3 / 2} className="relative w-full h-64">
+            <div className="relative w-full h-full">
+              <Image
+                src={Img1}
+                alt="Telkimisniit"
+                layout="fill"
+                className="rounded-md object-cover drop-shadow-2xl"
+                sizes="(min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw"
+                priority
+              />
+            </div>
+          </AspectRatio>
+          <AspectRatio ratio={3 / 2} className="relative w-full h-64">
+            <div className="relative w-full h-full">
+              <Image
+                src={Img2}
+                alt="Autoelamud"
+                layout="fill"
+                className="rounded-md object-cover drop-shadow-2xl"
+                sizes="(min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw"
+                priority
+              />
+            </div>
+          </AspectRatio>
+          <AspectRatio ratio={4 / 3} className="relative w-full h-64">
+            <div className="relative w-full h-full">
+              <Image
+                src={Img3}
+                alt="Kämpingumaja"
+                layout="fill"
+                className="rounded-md object-cover drop-shadow-2xl"
+                sizes="(min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw"
+                priority
+              />
+            </div>
+          </AspectRatio>
+          <AspectRatio ratio={4 / 3} className="relative w-full h-64">
+            <div className="relative w-full h-full">
+              <Image
+                src={Img4}
+                alt="Lihtne öömaja"
+                layout="fill"
+                className="rounded-md object-cover drop-shadow-2xl"
+                sizes="(min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw"
+                priority
+              />
+            </div>
+          </AspectRatio>
+          <AspectRatio ratio={4 / 3} className="relative w-full h-64">
+            <div className="relative w-full h-full">
+              <Image
+                src={Img5}
+                alt="Kodumajutuse toad"
+                layout="fill"
+                className="rounded-md object-cover drop-shadow-2xl"
+                sizes="(min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw"
+                priority
+              />
+            </div>
+          </AspectRatio>
+          <AspectRatio ratio={4 / 3} className="relative w-full h-64">
+            <div className="relative w-full h-full">
+              <Image
+                src={Img6}
+                alt="Puhkemaja"
+                layout="fill"
+                className="rounded-md object-cover drop-shadow-2xl"
+                sizes="(min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw"
+                priority
+              />
+            </div>
+          </AspectRatio>
         </div>
       </div>
     </section>
